54197ad50e2c92a5b53f9e5e4d49edcf3e54f44f
cbjoon <qudwns017@naver.com>
2023-08-28 20:55:56 +0900

    Math

---
+++ src/Math/_13458.java
@@ -0,0 +1,10 @@
+package Math;
+
+public class _13458 {
+    private void solution() {
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        new _13458().solution();
+    }
+}
---
+++ src/Math/_4375.java
@@ -0,0 +1,29 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class _4375 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        StringBuilder answer = new StringBuilder();
+        while ((s=br.readLine()) != null) {
+            BigInteger n = new BigInteger(s);
+            BigInteger base = BigInteger.ZERO;
+            while(true) {
+                base = base.multiply(BigInteger.TEN).add(BigInteger.ONE);
+                if (base.mod(n).equals(BigInteger.ZERO)) {
+                    answer.append(base.toString().length()).append('\n');
+                    break;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new _4375().solution();
+    }
+}
